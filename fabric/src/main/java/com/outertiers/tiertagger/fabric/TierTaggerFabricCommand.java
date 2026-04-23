3787d8690ac0b44f4fac304467f76fb9de7ae017
blackdmega-wq <blackdmega@gmail.com>
2026-04-23 20:42:59 +0200

    feat: nametag badges, fall-through, screens, multi-version matrix, quilt support

--- fabric/src/main/java/com/outertiers/tiertagger/fabric/TierTaggerFabricCommand.java
+++ fabric/src/main/java/com/outertiers/tiertagger/fabric/TierTaggerFabricCommand.java
@@ -3,8 +3,11 @@ package com.outertiers.tiertagger.fabric;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.outertiers.tiertagger.common.TierConfig;
 import com.outertiers.tiertagger.common.TierTaggerCore;
+import com.outertiers.tiertagger.fabric.screen.TierConfigScreen;
+import com.outertiers.tiertagger.fabric.screen.TierProfileScreen;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
 public class TierTaggerFabricCommand {
@@ -37,6 +40,23 @@ public class TierTaggerFabricCommand {
                             (TierTaggerCore.config().showInTab ? "§aon" : "§cdisabled")));
                         return 1;
                     }))
+                .then(ClientCommandManager.literal("nametag")
+                    .executes(ctx -> {
+                        TierTaggerCore.config().showNametag = !TierTaggerCore.config().showNametag;
+                        TierTaggerCore.config().save();
+                        ctx.getSource().sendFeedback(Text.literal("§a[TierTagger] §rNametag badges: " +
+                            (TierTaggerCore.config().showNametag ? "§aon" : "§cdisabled")));
+                        return 1;
+                    }))
+                .then(ClientCommandManager.literal("fallthrough")
+                    .executes(ctx -> {
+                        TierTaggerCore.config().fallthroughToHighest = !TierTaggerCore.config().fallthroughToHighest;
+                        TierTaggerCore.config().save();
+                        TierTaggerCore.cache().invalidate();
+                        ctx.getSource().sendFeedback(Text.literal("§a[TierTagger] §rFall-through to highest: " +
+                            (TierTaggerCore.config().fallthroughToHighest ? "§aon" : "§coff")));
+                        return 1;
+                    }))
                 .then(ClientCommandManager.literal("peak")
                     .executes(ctx -> {
                         TierTaggerCore.config().showPeak = !TierTaggerCore.config().showPeak;
@@ -62,15 +82,31 @@ public class TierTaggerFabricCommand {
                             ctx.getSource().sendFeedback(Text.literal("§a[TierTagger] §rAPI base: §e" + url));
                             return 1;
                         })))
+                .then(ClientCommandManager.literal("config")
+                    .executes(ctx -> {
+                        MinecraftClient.getInstance().send(() ->
+                            MinecraftClient.getInstance().setScreen(new TierConfigScreen(null)));
+                        return 1;
+                    }))
+                .then(ClientCommandManager.literal("profile")
+                    .then(ClientCommandManager.argument("player", StringArgumentType.word())
+                        .executes(ctx -> {
+                            String name = StringArgumentType.getString(ctx, "player");
+                            MinecraftClient.getInstance().send(() ->
+                                MinecraftClient.getInstance().setScreen(new TierProfileScreen(null, name)));
+                            return 1;
+                        })))
                 .executes(ctx -> {
                     TierConfig c = TierTaggerCore.config();
                     ctx.getSource().sendFeedback(Text.literal(
                         "§a[TierTagger] §rConfig:\n" +
                         " §7gamemode§r: §e" + c.gamemode + "\n" +
                         " §7tab badges§r: " + (c.showInTab ? "§aon" : "§coff") + "\n" +
+                        " §7nametag badges§r: " + (c.showNametag ? "§aon" : "§coff") + "\n" +
                         " §7show peak§r: " + (c.showPeak ? "§aon" : "§coff") + "\n" +
+                        " §7fallthrough§r: " + (c.fallthroughToHighest ? "§aon" : "§coff") + "\n" +
                         " §7api§r: §e" + c.apiBase + "\n" +
-                        "§7Subcommands: §fmode <name>, toggle, peak, refresh, api <url>"));
+                        "§7Subcommands: §fmode <name>, toggle, nametag, peak, fallthrough, refresh, api <url>, config, profile <player>"));
                     return 1;
                 })
             );
