5769da1f17b49e08a1e000b442f1b75db5f1bc71
blackdmega-wq <blackdmega@gmail.com>
2026-04-23 20:26:57 +0200

    feat(fabric): inject FabricLoader configDir into TierConfig

--- fabric/src/main/java/com/outertiers/tiertagger/fabric/TierTaggerFabric.java
+++ fabric/src/main/java/com/outertiers/tiertagger/fabric/TierTaggerFabric.java
@@ -1,11 +1,14 @@
 package com.outertiers.tiertagger.fabric;
 
+import com.outertiers.tiertagger.common.TierConfig;
 import com.outertiers.tiertagger.common.TierTaggerCore;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class TierTaggerFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        TierConfig.setConfigDir(FabricLoader.getInstance().getConfigDir());
         TierTaggerCore.init();
         TierTaggerFabricCommand.register();
     }
