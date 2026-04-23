6fd51ea83e118fca2c2fe98a1aad6748e8a76beb
blackdmega-wq <blackdmega@gmail.com>
2026-04-23 20:26:57 +0200

    feat(neoforge): inject FMLPaths configDir into TierConfig

--- neoforge/src/main/java/com/outertiers/tiertagger/neoforge/TierTaggerNeoForge.java
+++ neoforge/src/main/java/com/outertiers/tiertagger/neoforge/TierTaggerNeoForge.java
@@ -1,15 +1,18 @@
 package com.outertiers.tiertagger.neoforge;
 
+import com.outertiers.tiertagger.common.TierConfig;
 import com.outertiers.tiertagger.common.TierTaggerCore;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
 import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(value = TierTaggerCore.MOD_ID, dist = Dist.CLIENT)
 public class TierTaggerNeoForge {
     public TierTaggerNeoForge(IEventBus modBus) {
+        TierConfig.setConfigDir(FMLPaths.CONFIGDIR.get());
         TierTaggerCore.init();
         NeoForge.EVENT_BUS.addListener(this::onRegisterCommands);
     }
